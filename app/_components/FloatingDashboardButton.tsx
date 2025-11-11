2d4c8216e32a3b53357afab5719d85261ce9e0d2
ThinkMuk <b2000kang@gmail.com>
2025-11-11 20:42:49 +0900

    Enhance: FloatingDashBoardButton 클릭 시 WISE-PaaS 연결

--- app/_components/FloatingDashboardButton.tsx
+++ app/_components/FloatingDashboardButton.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+'use client';
+import React from 'react';
 
 export default function FloatingDashboardButton() {
   return (
-    <button className='w-full h-[68px] bg-[#FAFBFD] border-t rounded-t-4xl border-black/10'>
-        <div className='text-[18px] font-bold'>
-            <span className='text-[#17437C]'>WISE-PaaS /</span>
-            <span className='text-[#288DD7]'> Dashboard</span>
-        </div>
+    <button
+      onClick={() => {
+        window.open('https://dashboard-inha2025-ews.education.wise-paas.com/?orgId=5', '_blank', 'noopener,noreferrer');
+      }}
+      className='w-full h-[68px] bg-[#FAFBFD] border-t cursor-pointer rounded-t-4xl border-black/10 hover:bg-[#e8f0fa] transition-colors duration-200'
+    >
+      <div className='text-[18px] font-bold'>
+        <span className='text-[#17437C]'>WISE-PaaS /</span>
+        <span className='text-[#288DD7]'> Dashboard</span>
+      </div>
     </button>
-  )
+  );
 }
