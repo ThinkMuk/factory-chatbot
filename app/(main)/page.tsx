38ab60cbb5dee0ad43a0042ad2881626e4c21a32
ThinkMuk <b2000kang@gmail.com>
2025-11-26 17:03:28 +0900

    Refactor: (main) 라우트 그룹 적용으로 홈페이지 구조화

---
+++ app/(main)/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import Link from 'next/link';
+import ChatRoomList from '@/app/_components/ChatRoomList';
+
+export default function Home() {
+  return (
+    <div className='h-full flex flex-col relative'>
+      <main className='flex-1 min-h-0'>
+        <ChatRoomList useLink />
+      </main>
+      <Link
+        href='/chat/new'
+        className='absolute bottom-5 left-4 w-[50px] h-[50px] rounded-full bg-[#194268] text-white flex items-center justify-center text-2xl shadow-lg'
+        aria-label='새 채팅 시작'
+      >
+        +
+      </Link>
+    </div>
+  );
+}
+
